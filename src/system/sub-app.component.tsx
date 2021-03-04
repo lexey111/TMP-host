21884fde94282687ded26e86f649f94b8463183d
lexey111 <lexey111@gmail.com>
2021-03-04 18:43:34 +0200

    Better mounting component

--- src/system/sub-app.component.tsx
+++ src/system/sub-app.component.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-console,@typescript-eslint/no-unsafe-assignment,prefer-destructuring,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access */
 import * as React from 'react';
-import {useEffect, useRef, useState} from 'react';
+import {useLayoutEffect, useRef, useState} from 'react';
 import {ITmpManager} from 'TMPCore/index';
 
 export type TSubAppProps = {
@@ -9,42 +9,74 @@ export type TSubAppProps = {
 	silent?: boolean
 };
 
-let id = 0;
+let id = 1;
 
 export const SubApp: React.FC<TSubAppProps> = (props: TSubAppProps) => {
 	const ref = useRef<HTMLDivElement>();
+	const destroying = useRef(false);
 	const [prepared, setPrepared] = useState(false);
+	const mountId = useRef('');
+	const mountedId = useRef('');
 
-	useEffect(() => {
-		// allow to mount only if application is known
+	useLayoutEffect(() => {
+		return () => {
+			destroying.current = true;
+
+			if (mountedId.current) {
+				const TmpManager: ITmpManager = window['TmpManager'];
+				TmpManager.unmount(props.subappView, mountedId.current);
+			}
+		};
+	}, []);
+
+	useLayoutEffect(() => {
+		if (destroying.current) {
+			return;
+		}
+		// allow to mount only if application is known and enabled
 		const subappsAvailable = window['TmpCore']['environment']['subAppList'];
 		const bundleRequested = (props.subappView || '').split('/')[0];
 
-		setPrepared(Boolean(subappsAvailable[bundleRequested]) && subappsAvailable[bundleRequested].enabled);
+		if (!(Boolean(subappsAvailable[bundleRequested]) && subappsAvailable[bundleRequested].enabled)) {
+			return;
+		}
+
+		mountId.current = 'tmp-container-' + (id++).toString();
+
+		// asynchronous rendering
+		setTimeout(() => {
+			if (destroying.current) {
+				return;
+			}
+			setPrepared(true);
+		}, 20);
 	}, []);
 
-	useEffect(() => {
-		if (!ref.current || !prepared) {
+	useLayoutEffect(() => {
+		if (!ref.current || !prepared || destroying.current || mountedId.current) {
 			return;
 		}
 
 		const TmpManager: ITmpManager = window['TmpManager'];
-		const currentId = ref.current.id;
 
-		TmpManager
-			.mount(props.subappView, currentId)
-			.catch(err => {
-				if (props.silent !== true) {
-					console.log('Error on mounting', props.subappView);
-					console.log(err);
-				} else {
-					setPrepared(false);
-				}
-			});
-
-		return () => {
-			TmpManager.unmount(props.subappView, currentId);
-		};
+		try {
+			TmpManager
+				.mount(props.subappView, mountId.current)
+				.then(() => {
+					mountedId.current = mountId.current;
+					return void 0;
+				})
+				.catch(err => {
+					if (props.silent !== true) {
+						console.log('Error on mounting', props.subappView);
+						console.log(err);
+					} else {
+						setPrepared(false);
+					}
+				});
+		} catch {
+			//
+		}
 	}, [ref, prepared]);
 
 	if (!prepared) {
@@ -53,7 +85,7 @@ export const SubApp: React.FC<TSubAppProps> = (props: TSubAppProps) => {
 
 	return <div
 		ref={ref}
-		id={'tmp-container-' + (id++).toString()}
+		id={mountId.current}
 		className={'tmp-subapp-view' + (props.className ? ' ' + props.className : '')}>
 	</div>;
 };
